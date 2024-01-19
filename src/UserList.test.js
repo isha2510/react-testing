3382854a69605944921fca066dc7033e02dee18e
isha2510 <ishatripathi80@gmail.com>
2024-01-19 18:02:42 +0000

    Added test to verify name and email in UserList

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -8,12 +8,30 @@ test('render one row per user', () => {
     ];
     render(<UserList users={users} />);
 
-    const rows=within(screen.getByTestId('users')).getAllByRole('row');
+    //eslint-disable-next-line
+    //const rows = container.querySelectorAll('tbody tr');
+
+    const rows = within(screen.getByTestId('users')).getAllByRole('row');
 
     expect(rows).toHaveLength(2);
 
 });
 
 test('render the email and name of each user', () => {
-    render(<UserList />)
+    const users = [
+        { name: 'test', email: 'test@test.com' },
+        { name: 'test1', email: 'test1@test.com' }
+    ];
+    render(<UserList users={users} />);
+
+    //Below line will generate playground link, it helps to find the query for specific element
+    // screen.logTestingPlaygroundURL();
+
+    for (let user of users) {
+        const name = screen.getByRole('cell', { name: user.name });
+        const email = screen.getByRole('cell', { name: user.email });
+        expect(name).toBeInTheDocument();
+        expect(email).toBeInTheDocument();
+
+    }
 })
